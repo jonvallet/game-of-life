754ee95c2506b369c4ae244b4ccafb6c5ca6cea6
Jon Vallet <jon.vallet@proxama.com>
2015-04-23 15:15:50 +0100

    Added show and generate functions

--- src/main/scala/com/jonvallet/scala/life/Life.scala
+++ src/main/scala/com/jonvallet/scala/life/Life.scala
@@ -3,6 +3,19 @@ package com.jonvallet.scala.life
 /**
  * @author Jon Vallet
  */
-class Life {
+object Life {
+
+  def generate(rows: Int, cols: Int): List[List[Boolean]] = {
+    def random = math.random * 10 toInt
+
+    List.fill(10,10)(random % 5 == 0)
+  }
+
+
+
+
+  def show(game: List[List[Boolean]]): Unit = {
+    for (row <- game) println(row map { case true => "█" case false => "░" } mkString (" "))
+  }
 
 }
---
+++ src/test/scala/com/jonvallet/scala/life/Life.sc
@@ -0,0 +1,6 @@
+import com.jonvallet.scala.life.Life
+
+
+val game = Life.generate(10,10)
+
+Life.show(game)
